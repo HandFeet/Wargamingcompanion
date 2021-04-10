f28839960be43e802b88defad0bddd83c67da57d
david <cd4ve@hotmail.co.uk>
2021-04-10 13:14:29 +0100

    work on layout for game screen

--- app/src/main/java/co/uk/outlook/davidslambert/warcompanion/presention/game/GameActivity.kt
+++ app/src/main/java/co/uk/outlook/davidslambert/warcompanion/presention/game/GameActivity.kt
@@ -3,16 +3,10 @@ package co.uk.outlook.davidslambert.warcompanion.presention.game
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import co.uk.outlook.davidslambert.warcompanion.R
-import co.uk.outlook.davidslambert.warcompanion.presention.game.ui.main.GameFragment
 
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.game_activity)
-        if (savedInstanceState == null) {
-            /*supportFragmentManager.beginTransaction()
-                .replace(R.id.container, GameFragment.newInstance())
-                .commitNow()*/
-        }
     }
 }
